240736805ddd8aa458e4eaeadea124d0b1b0a502
Jiaqigeorgeli <44448607+Jiaqigeorgeli@users.noreply.github.com>
2018-11-16 17:55:13 -0600

    Update linetrack.py

--- linetrack.py
+++ linetrack.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
+from picamera.array import PiRGBArray
+from picamera import PiCamera
 import sys
+import argparse
+import warnings
+import datetime
 import time
 import cv2
 import numpy as np
 import os
+import imutils
 
 Kernel_size=15
 low_threshold=40
@@ -16,12 +22,16 @@ minLineLength=10
 maxLineGap=1
 
 #Initialize camera
-video_capture = cv2.VideoCapture(0)
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
 
-while True:
+for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     # CAPTURE FRAME-BY-FRAME
-    ret, frame = video_capture.read()
+    frame = f.array
     time.sleep(0.1)
+    #frame = imutils.resize(frame, width=500)
     #Convert to Grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     #Blur image to reduce noise. if Kernel_size is bigger the image will be more blurry
@@ -42,9 +52,9 @@ while True:
     cv2.circle(frame,(320,240),2,(255,0,0),2)
     
     #With this for loops only a dots matrix is painted on the picture
-    #for y in range(0,480,20):
-            #for x in range(0,640,20):
-                #cv2.line(frame,(x,y),(x,y),(0,255,255),2)
+    for y in range(0,480,20):
+            for x in range(0,640,20):
+                cv2.line(frame,(x,y),(x,y),(0,255,255),2)
     
     #With this for loops a grid is painted on the picture
     for y in range(0,480,40):
@@ -53,16 +63,24 @@ while True:
                 cv2.line(frame,(x,0),(x,480),(255,0,0),1)
                 
     #Draw lines on input image
-    if(lines != None):
+    # if ( len(lines) != 0 )
+    #print(lines);
+    if(lines is not None):
         for x1,y1,x2,y2 in lines[0]:
             cv2.line(frame,(x1,y1),(x2,y2),(0,255,0),2)
             cv2.putText(frame,'lines_detected',(50,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),1)
-    cv2.imshow("line detect test", frame)
-
+        cv2.imshow("line detect test", frame)
+    else:
+        cv2.putText(frame,'lines_undetected',(50,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),1)
+        cv2.imshow("line detect test", frame)
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
+    
+ 
 # When everything is done, release the capture
 
-video_capture.release()
+#video_capture.release()
 cv2.destroyAllWindows()
